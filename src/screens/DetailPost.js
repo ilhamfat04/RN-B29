fbf3f49e9a8f1d2bd08d6e0d5a179eccb53a16b5
= <=>
2022-03-30 00:53:25 +0700

    update

--- src/screens/DetailPost.js
+++ src/screens/DetailPost.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -10,25 +10,70 @@ import {
 import { ListItem, Avatar } from "react-native-elements";
 
 // Import Axios
+import axios from "axios";
 
 const PostDetail = (props) => {
   //init Props
+  const title = props.route.params.title;
+  const body = props.route.params.body;
+  const id = props.route.params.id;
 
   //Init State
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   // Create LifeCycle
-  //Function Exception
+  useEffect(() => {
+    //Function Exception
+    getComment();
+  }, []);
 
   // Create Function to fetch
+  const getComment = () => {
+    setIsLoading(true);
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+      .then((res) => {
+        setComments(res.data);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        alert("Error Get Comments");
+        setIsLoading(false);
+      });
+  };
 
   //   Create Component List
+  const _renderItem = ({ item }) => {
+    return (
+      <ListItem key={item.id.toString()} bottomDivider>
+        <ListItem.Content>
+          <ListItem.Title numberOfLines={1}>{item.email}</ListItem.Title>
+          <ListItem.Subtitle
+            numberOfLines={2}
+          >{`${item.name} - ${item.body}`}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    );
+  };
+
   return (
     <View style={style.container}>
       <Text h2 style={{ fontWeight: "bold" }}>
-        This Is Post Detail
+        {title}
       </Text>
       {/* Implement Axios Here */}
       {/* Render Component List */}
+      <Text style={{ marginTop: 20 }}>{body}</Text>
+      <Text style={{ marginTop: 20, color: "grey" }}>Comments</Text>
+      <FlatList
+        data={comments}
+        renderItem={_renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        refreshControl={
+          <RefreshControl refreshing={isLoading} onRefresh={getComment} />
+        }
+      />
     </View>
   );
 };
